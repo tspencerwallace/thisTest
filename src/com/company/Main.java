4ff04a51bb5a186c2a94a9b6b433ff8642f5c779
Tucker <Me>
2016-05-26 14:11:34 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("fart");
+        if(true)
+        {
+            int sum = 0;
+        }
+    }
+}
